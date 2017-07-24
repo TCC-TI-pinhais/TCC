fd3ca6c84ec1071a6003154c377634b7f7a29f48
Rodrigo <Rodrigo@192.168.223.1>
2017-07-24 11:16:07 -0300

    V1.2

--- src/control/LoginController.java
+++ src/control/LoginController.java
@@ -7,7 +7,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
 import model.TCC;
 
 public class LoginController implements Initializable {
